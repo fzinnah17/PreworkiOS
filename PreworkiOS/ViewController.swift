d884cb1f9d3c3a9698f854e5cab1aab834cf93e9
fzinnah17 <fzinnah000@citymail.cuny.edu>
2022-07-25 20:37:47 -0400

    UI done

--- PreworkiOS/ViewController.swift
+++ PreworkiOS/ViewController.swift
@@ -9,11 +9,24 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var billAmountTextField: UITextField!
+    
+    @IBOutlet weak var tipAmountLabel: UILabel!
+    
+    @IBOutlet weak var tipControl: UISegmentedControl!
+    
+    
+    @IBOutlet weak var totalLabel: UILabel!
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func calculateTip(_ sender: Any) {
+    }
+    
 }
 
