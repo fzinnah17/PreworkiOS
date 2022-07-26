db2442f334ca678a19c1822f0476b2b5827318c8
fzinnah17 <fzinnah000@citymail.cuny.edu>
2022-07-25 20:50:54 -0400

    Completed

--- PreworkiOS/ViewController.swift
+++ PreworkiOS/ViewController.swift
@@ -26,6 +26,16 @@ class ViewController: UIViewController {
     }
 
     @IBAction func calculateTip(_ sender: Any) {
+        let bill = Double(billAmountTextField.text!) ?? 0
+        
+        let tipPercentages = [0.15, 0.18, 0.2]
+        
+        let tip = bill*tipPercentages[tipControl.selectedSegmentIndex]
+        
+        let total = bill + tip
+        
+        tipAmountLabel.text = String(format: "$%.2f", tip)
+        totalLabel.text = String(format: "$%.2f", total)
     }
     
 }
